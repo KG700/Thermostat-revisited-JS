ba96d2f00f0819a0e90c5f87212cd54f2eaf898f
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-07 15:29:42 +0100

    Linked up reset button to thermostat.resetTemperature()

--- interface.js
+++ interface.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
+
   $('#temperature-up').on('click', function() {
     thermostat.up();
     $('#temperature').text(thermostat.temperature);
   });
+
   $('#temperature-down').on('click', function() {
     thermostat.down();
+    $('#temperature').text(thermostat.temperature);
+  })
+
+  $('#temperature-reset').on('click', function() {
+    thermostat.resetTemperature();
     $('#temperature').text(thermostat.temperature);
   })
 
