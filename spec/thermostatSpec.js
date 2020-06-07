b0596d3fd6978efc11cd9675e4403aca3d1a6c7d
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-07 13:50:06 +0100

    Passing test: implemented power saving mode to be on by default

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -27,5 +27,9 @@ describe('Thermostat', function() {
       thermostat.down();
     }
     expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('has power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
   })
 });
