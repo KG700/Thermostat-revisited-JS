887c191bb77fbb5377a703d7c726054f3ac841a3
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-07 15:26:48 +0100

    Linked up - button to thermostat.down()

--- interface.js
+++ interface.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
   $('#temperature-up').on('click', function() {
     thermostat.up();
     $('#temperature').text(thermostat.temperature);
+  });
+  $('#temperature-down').on('click', function() {
+    thermostat.down();
+    $('#temperature').text(thermostat.temperature);
   })
 
 });
