a644329dccf1f44537a51198f49204ce56cb74ff
Karla Gardiner <karla.gardiner@gmail.com>
2020-06-07 16:16:26 +0100

    Connected to open weather api. Can now display temperature from different cities

--- interface.js
+++ interface.js
@@ -29,8 +29,29 @@ $(document).ready(function() {
     updateTemperature()
   });
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
+
+  displayWeather('London');
+
+  $('#current-city').change(function(event) {
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
   }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  };
 
 });
